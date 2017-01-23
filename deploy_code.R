05f56aaa9308ad3b2521e90c80fe3f2137d8ff17
choux130 <choux130@umn.du>
2017-01-23 14:47:15 -0500

    test on knitr-jekyll

---
+++ deploy_code.R
@@ -0,0 +1,9 @@
+install.packages(c("servr", "knitr"), repos = "http://cran.rstudio.com")
+
+library(servr)
+jekyll(dir = ".", input = c(".", "_source", "_posts"),
+       output = c(".", "_posts", "_posts"),
+       script = c("Makefile", "build.R"),
+       serve = TRUE,
+       command = "bundle exec jekyll build")
+# command = "jekyll build"
