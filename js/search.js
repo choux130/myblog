c2cf6b70f391c572f728db45f1f3d0adbdc362e8
choux130 <choux130@umn.du>
2017-02-13 09:47:26 -0500

    search

--- js/search.js
+++ js/search.js
@@ -42,7 +42,7 @@ jQuery(function() {
         results.forEach(function(result) {
           var item = loaded_data[result.ref];
           // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' +item.url + '  "> ' + item.title + '</a></li>';
+          var appendString = '<li><a href="/myblog' +item.url.replace(/\s/g,'')+  '  "> ' + item.title + '</a></li>';
 
           // Add the snippet to the collection of results.
           $search_results.append(appendString);
