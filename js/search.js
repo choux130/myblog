b03e097e3d1f2059fd2470bc45f8afe18ad705ea
choux130 <choux130@umn.du>
2017-03-28 15:31:35 -0400

    update

--- js/search.js
+++ js/search.js
@@ -42,7 +42,8 @@ jQuery(function() {
         results.forEach(function(result) {
           var item = loaded_data[result.ref];
           // Build a snippet of HTML for this result
-          var appendString = '<li><a href="/myblog' +item.url.replace(/\s/g,'')+  '  "> ' + item.title + '</a></li>';
+          var appendString = '<li><a href="' +item.url+  '"> ' + item.title + '</a></li>';
+          // origin: '<li><a href="' + item.url + '">' + item.title + '</a></li>'
           // .replace(/\s/g,'') can remove the space at the beginning
 
           // Add the snippet to the collection of results.
